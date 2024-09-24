903ab0f9649713ea9e69e937e25a232dd2e0e702
snehapatidar5 <sneha.mongooseintern@gmail.com>
2024-09-24 15:36:52 +0530

    Update Video.jsx

--- src/Component/OtherComponents/Video.jsx
+++ src/Component/OtherComponents/Video.jsx
@@ -1,7 +1,7 @@
 import video from '../../assets/video.png'
 function Video() {
   return (
-    <div className='lg:max-2xl:flex lg:max-2xl:justify-evenly sm:text-center sm:my-10 sm:flex sm:flex-col-reverse md:flex-row '>
+    <div className='lg:max-2xl:flex lg:max-2xl:mt-[-40px] lg:max-2xl:justify-center   lg:max-2xl:text-left sm:text-center sm:my-4 sm:flex sm:flex-col-reverse md:flex-row '>
     <div className='lg:max-2xl:w-[513px] lg:max-2xl:p-4 lg:max-2xl:mt-[180px]  sm:h-[280px] '>
         <h2 className='text-[#12171B]  lg:max-2xl:text-[40px] sm:text-[24px] font-bold '>Why Choose Kai-Zen®?</h2>
         <p className='text-[#3A3939] text-[16px] sm:py-2'>We have all used leverage to purchase a nicer house or to buy a better car. Kai-Zen® uses leverage for the potential to accumulate more growth & obtain more protection, while providing the ability to maintain your current standard of living. That’s the smart way to use leverage.</p> 
@@ -9,7 +9,7 @@ function Video() {
 
           </div>
     <div className='lg:max-2xl:my-[90px] sm:my-4 sm:p-2'>
-        <img src={video} className='lg:max-2xl:w-[650px] lg:max-2xl:h-[450px] sm:w-[390px] sm:h-[280px]'/>
+        <img src={video} className='lg:max-2xl:w-[535px] lg:max-2xl:h-[400px] sm:w-[390px] sm:h-[280px]'/>
     </div>
     </div>
   )
