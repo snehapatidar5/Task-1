2608711db75da53193108863799c71084b999922
snehapatidar5 <sneha.mongooseintern@gmail.com>
2024-09-24 15:34:30 +0530

    Update Cards.jsx

--- src/Component/OtherComponents/Cards.jsx
+++ src/Component/OtherComponents/Cards.jsx
@@ -23,7 +23,7 @@ const data = [
 
 function Cards() {
   return (
-    <div className='sm:mt-14 '>
+    <div className='sm:mt-[380px] lg:max-2xl:mt-[-30px] '>
       <div className='text-center  '>
         <h2 className='text-[#12171B]  lg:max-2xl:text-[40px] sm:text-[30px] font-semibold '>Benefits of Kai-Zen®</h2>
         <p className='text-[#3A3939] text-[16px] lg:max-2xl:w-[667px] lg:max-2xl:h-[90px] sm:p-1 text-center lg:max-2xl:ml-[350px]'>We have all used leverage to purchase a nicer home, or to buy a better car. Kai-Zen® uses leverage for the potential to accumulate cash, providing up to 60 - 100% more benefits, than using your money alone.</p>
@@ -31,11 +31,11 @@ function Cards() {
 
 
        
-          <div className='lg:max-2xl:flex lg:max-2xl:justify-around '>
+          <div className='lg:max-2xl:flex lg:max-2xl:justify-center  '>
             {data.map((item, index) => (
-              <div key={index} className="bg-[#e1e3ec] lg:max-2xl:w-[330px] sm:w-[320px]  sm:h-[320px] lg:max-2xl:h-[360px]  rounded-lg lg:max-2xl:p-3 sm:my-4 sm:p-2 sm:ml-6 ">
+              <div key={index} className="bg-[#e1e3ec] lg:max-2xl:w-[330px] sm:w-[320px]  sm:h-[340px] lg:max-2xl:h-[360px] p-4 rounded-lg  sm:my-4  sm:ml-6 ">
                 <img className='w-[68px] h-[68px]' src={item.img} alt="Card" />
-                <h2 className='text-[25px] font-semibold lg:max-2xl:py-2 text-[#12171B]'>{item.heading}</h2>
+                <h2 className='text-[25px] font-semibold lg:max-2xl:py-2 text-[#12171B] h-[130px] '>{item.heading}</h2>
                 <p className='text-[#3A3939] text-[16px]'>{item.para}</p>
               </div>
             ))}
