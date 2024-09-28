7522dd7abbdbbedfe6b1c07d37a7dffff736a172
snehapatidar5 <sneha.mongooseintern@gmail.com>
2024-09-28 12:37:43 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,46 +7,17 @@ export default {
   theme: {
     extend: {
       screens:{
-        sm:"375px",
-        md:'600px'
+        'xs':'360px',
+        'sm': '640px',  // Default small size
+        'md': '768px',  // Default medium size
+        'lg': '1024px', // Default large size
+        'xl': '1280px', // Default extra large size
+        '2xl': '1536px', // Default 2x large size
+        '3xl': '1800px', // Custom size for large desktops
+        
       },
     },
   },
   plugins: [],
 }
 
-// import withMT from "@material-tailwind/react/utils/withMT";
- 
-// export default withMT({
-//   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {
-//       screens:{
-//         sm:"370px",
-//         md:"600px",
-//       },
-//     },
-//   },
-//   plugins: [],
-// });
-
-// const withMT = require("@material-tailwind/react/utils/withMT");
- 
-// module.exports = withMT({
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{vue,js,ts,jsx,tsx}",
-//     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-//     "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {
-//       screens:{
-//         sm:"370px",
-//         md:"600px",
-//       },
-       
-//     },
-//   },
-//   plugins: [],
-// });
