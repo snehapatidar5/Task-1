42e90143712888a909bd03661945a451f6ec0bd2
snehapatidar5 <sneha.mongooseintern@gmail.com>
2024-09-28 11:50:45 +0530

    Update Navbar.jsx

--- src/Component/Header/Navbar.jsx
+++ src/Component/Header/Navbar.jsx
@@ -4,46 +4,51 @@ import { CiMail } from "react-icons/ci";
 function Navbar() {
   return (
     <>
-      <nav className=" bg-[rgba(10,44,82,1)] lg:max-2xl:h-[65.59px] sm:h-[65.59px] sm:w-[100%]  lg:max-2xl:w-[100%] lg:max-2xl:pt-0  sm:pt-2.5">
-        <div className="flex ">
-          <div className="flex lg:max-2xl:mt-[12px] lg:max-2xl:ml-20 lg:max-2xl:w-[55%]  lg:max-2xl:gap-[18px] sm:gap-4 sm:w-[100%]">
-            <img src={logo} className="lg:max-2xl:ml-[11px] sm:ml-[15px] w-[40px] h-[40px]" />
+      <nav className="  bg-[rgba(10,44,82,1)]  h-[65.59px] w-[100%]  lg:max-3xl:pt-0  xs:pt-2.5 sm:pt-0 ">
+         <div className="flex lg:mx-8 ">
+          <div className="flex  lg:max-3xl:mt-[12px]  lg:max-3xl:w-[55%]  lg:max-3xl:gap-[18px] xs:gap-3 xs:max-lg: xs:max-lg:w-[100%] sm:mt-3 ">
+            <img src={logo} className="lg:max-3xl:ml-[11px] xs:ml-[15px] w-[40px] h-[40px]" />
             
-            <div className='flex lg:max-2xl:gap-4 lg:max-2xl:mt-2 sm:m-0 sm:gap-1' >
-            <div className="sm-w-[100px]   sm:m-0">
-            <span className="text-white font-bold lg:max-2xl:text-lg sm:text-base sm:leading-none">
+            <div className='flex lg:gap-1 lg:max-3xl:mt-2 xs:m-0 xs:gap-4 sm:gap-2 sm:mt-2 ' >
+            <div className="xs-w-[100px] sm:w-[230px] md:w-[265px] lg:w-[350px]   xs:m-0">
+            <span className=" md:text-sm text-white font-bold lg:max-3xl:text-lg xs:text-base sm:text-xs xs:leading-none">
               Discover Benefits of Kai-Zen® with Jane
             </span>
             </div>
              
-             <div className=' sm:mr-2'>
-             <p className="cursor-pointer underline font-bold text-[#7592F2] lg:max-2xl:text-lg sm:text-base sm:w-[100px] lg:max-2xl:mt-0 sm:mt-2.5" >
+             <div className=' lg:mr-[-10px]  xs:w-[150px] lg:w-[120px] sm:mr-[-30px] xs:mr-0'>
+             <span className=" md:text-sm cursor-pointer underline font-bold sm:text-xs text-[#7592F2] lg:max-3xl:text-lg xs:text-base  lg:max-3xl:mt-0 xs:m-0 sm:m-1 md:mt-1" >
               Learn More
-            </p>
+            </span>
              </div>
            </div>
           </div>
 
-          <div className="lg:max-2xl:flex lg:max-2xl:justify-end lg:max-2xl:mt-[6px] lg:max-2xl:mr-[100px] lg:max-2xl:w-[55%] sm:hidden">
+          <div className=" sm:gap-2  lg:max-3xl:flex lg:max-3xl:justify-end lg:max-3xl:mt-[6px] lg:max-3xl:w-[55%] sm:mt-5 sm:w-[100%] sm:flex sm:justify-center xs:hidden">
 
-            <div className="lg:max-2xl:flex lg:max-2xl:gap-[10px] lg:max-2xl:m-4">
-            <div className=' bg-white bg-opacity-20 p-1 lg:max-2xl:w-[38px] lg:max-2xl:h-[38px] rounded-full lg:max-2xl:mt-[-6px]'>
-            <BsTelephone  className="text-white lg:max-2xl:w-[22px] m-1 lg:max-2xl:h-[22px]  lg:max-2xl:my-1" />
-            </div>
-              <p className="text-white text-lg ">800-294-9940</p>
-            </div>
+          
 
-            <div className="lg:max-2xl:flex lg:max-2xl:gap-[10px] lg:max-2xl:m-4">
-            <div className=' bg-white bg-opacity-20 p-1 lg:max-2xl:w-[38px] lg:max-2xl:h-[38px] rounded-full lg:max-2xl:mt-[-6px]' >
-            <CiMail className="text-white lg:max-2xl:w-[29px] lg:max-2xl:h-[29px]"/>
+            <div className="flex lg:max-3xl:gap-[10px] sm:gap-2 lg:max-3xl:m-4">
+            <div className=' bg-white bg-opacity-20 lg:max-3xl:p-1 sm:w-[30px] sm:h-[30px] lg:max-3xl:w-[38px] lg:max-3xl:h-[38px]  sm:p-[7px] rounded-full lg:max-3xl:mt-[-6px]' >
+            <BsTelephone  className="text-white lg:max-3xl:w-[23px] lg:m-[2px] lg:max-3xl:h-[29px]" />
 
             </div>
-              <p  className="mb-2 text-white lg:max-2xl:text-lg">support@abc.com</p>
+              <p  className=" text-white md:text-sm lg:max-3xl:text-lg lg:max-3xl:mt-0 sm:text-xs sm:mt-1">800-294-9940</p>
+            </div>
+
+            <div className="flex lg:max-3xl:gap-[10px] sm:gap-2 lg:max-3xl:m-4">
+            <div className=' bg-white bg-opacity-20 lg:max-3xl:p-1 sm:w-[30px] sm:h-[30px] lg:max-3xl:w-[38px] lg:max-3xl:h-[38px]  sm:p-[7px] rounded-full lg:max-3xl:mt-[-6px]' >
+            <CiMail className="text-white lg:max-3xl:w-[29px] lg:max-3xl:h-[29px]"/>
+
+            </div>
+              <p  className=" text-white md:text-sm lg:max-3xl:text-lg lg:max-3xl:mt-0 sm:text-xs sm:mt-1">support@abc.com</p>
             </div>
           </div>
 
         </div>
       </nav>
+
+      
     </>
   );
 }
