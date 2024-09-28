acb8f6529150241b08c3e91ebcd494c86ea5d98a
snehapatidar5 <sneha.mongooseintern@gmail.com>
2024-09-28 11:54:48 +0530

    Update Questions.jsx

--- src/Component/OtherComponents/Questions.jsx
+++ src/Component/OtherComponents/Questions.jsx
@@ -2,13 +2,7 @@ import chart from '../../assets/chart.png'
 import chart2 from '../../assets/chart2.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown,faAngleUp } from '@fortawesome/free-solid-svg-icons';
-
-
 import  { useState } from 'react';
-
-
-
-
 
 
 const faqData  = [
@@ -39,20 +33,20 @@ const Questions = () => {
 
 
   return (
-    <div className='lg:max-2xl:mt-[-300px] sm:mt-[-400px]'>
-    <div >
-    <img src={chart} className='rounded-3xl p-2 cursor-pointer sm:hidden lg:max-2xl:block w-[100%]'/>
-    <img src={chart2} className='rounded-3xl p-2 cursor-pointer lg:max-2xl:hidden'/>
+    <div className='lg:max-3xl:mt-[70px] xs:mt-[40px] sm:mt-[40px] md:mt-10'>
+    <div className='flex justify-center'>
+    <img src={chart} className='rounded-3xl p-2 cursor-pointer xs:max-md:hidden md:max-3xl:block w-[100%] object-cover  h-auto'/>
+    <img src={chart2} className='sm:w-[700px] sm:h-[650px] rounded-3xl p-2 cursor-pointer md:max-3xl:hidden w-full  md:w-1/3  object-cover h-auto'/>
  </div>
- <div className="lg:max-2xl:max-w-3xl mx-auto  p-6 sm:max-w-lg  ">
-      <h2 className="text-center lg:max-2xl:text-3xl sm:text-2xl font-semibold mb-6">
+ <div className="lg:max-3xl:max-w-3xl mx-auto  p-6 xs:max-w-lg  ">
+      <h2 className="text-center lg:max-3xl:text-3xl xs:text-2xl font-semibold mb-6">
         Frequently Asked Questions
       </h2>
-      <div className="sm:space-y-2 lg:max-2xl:space-y-6 ">
+      <div className="xs:space-y-2 lg:max-3xl:space-y-6 ">
         {faqData.map((faq, index) => (
           <div key={index} className="bg-white shadow-md rounded-md">
             <button
-              className="w-full text-left lg:max-2xl:py-4 sm:p-4 font-semibold text-lg flex justify-between items-center focus:outline-none"
+              className="w-full text-left lg:max-3xl:py-4 xs:p-4 font-semibold text-lg flex justify-between items-center focus:outline-none"
               onClick={() => toggleFAQ(index)}
             >
               {faq.question}
